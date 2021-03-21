21f663edda55a04834148e1ac171908b65de4374
github username <github email address>
2021-03-20 21:58:34 -0500

    commit tree d49c048f194ef11dda1dd96e69b75b9f2fbff5f7

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,19 @@ class StudentsController < ApplicationController
   end
 
   def new
+    erb :views/students/'/new.html.erb'
   end
 
   def create
+    student = Student.create
+    student.first_name= params[:first_name]
+    student.last_name= params[:last_name]
+    student.save
+    redirect_to student_path(@student)
   end
+
+  def post
+  end
+
 
 end
